a7d5417f111420b41b5f8c59ca792ba796ddcd22
neutrinos <>
2021-02-10 11:59:50 +0530

    {/src/main/java/com/myspace/azurepoc/address.java}

--- src/main/java/com/myspace/azurepoc/address.java
+++ src/main/java/com/myspace/azurepoc/address.java
@@ -6,12 +6,47 @@ package com.myspace.azurepoc;
 
 public class address implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public address() {
-    }
+	@org.kie.api.definition.type.Label(value = "streeName")
+	private java.lang.String streetName;
+	@org.kie.api.definition.type.Label(value = "country")
+	private java.lang.String country;
+	@org.kie.api.definition.type.Label(value = "state")
+	private java.lang.String state;
 
+	public address() {
+	}
 
+	public java.lang.String getStreetName() {
+		return this.streetName;
+	}
 
+	public void setStreetName(java.lang.String streetName) {
+		this.streetName = streetName;
+	}
+
+	public java.lang.String getCountry() {
+		return this.country;
+	}
+
+	public void setCountry(java.lang.String country) {
+		this.country = country;
+	}
+
+	public java.lang.String getState() {
+		return this.state;
+	}
+
+	public void setState(java.lang.String state) {
+		this.state = state;
+	}
+
+	public address(java.lang.String streetName, java.lang.String country,
+			java.lang.String state) {
+		this.streetName = streetName;
+		this.country = country;
+		this.state = state;
+	}
 
 }

5c00148cc58a13e7b63abff8f6a2a7fa5cf6311b
neutrinos <>
2021-02-12 11:41:12 +0530

    {/src/main/java/com/myspace/azurepoc/vehicleDetails.java}

--- src/main/java/com/myspace/azurepoc/vehicleDetails.java
+++ src/main/java/com/myspace/azurepoc/vehicleDetails.java
@@ -6,12 +6,47 @@ package com.myspace.azurepoc;
 
 public class vehicleDetails implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public vehicleDetails() {
-    }
+	@org.kie.api.definition.type.Label(value = "vehicleNumber")
+	private java.lang.String vehicleNumber;
+	@org.kie.api.definition.type.Label(value = "driverName")
+	private java.lang.String driverName;
+	@org.kie.api.definition.type.Label(value = "contactNumber")
+	private java.lang.String contactNumber;
 
+	public vehicleDetails() {
+	}
 
+	public java.lang.String getVehicleNumber() {
+		return this.vehicleNumber;
+	}
 
+	public void setVehicleNumber(java.lang.String vehicleNumber) {
+		this.vehicleNumber = vehicleNumber;
+	}
+
+	public java.lang.String getDriverName() {
+		return this.driverName;
+	}
+
+	public void setDriverName(java.lang.String driverName) {
+		this.driverName = driverName;
+	}
+
+	public java.lang.String getContactNumber() {
+		return this.contactNumber;
+	}
+
+	public void setContactNumber(java.lang.String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
+	public vehicleDetails(java.lang.String vehicleNumber,
+			java.lang.String driverName, java.lang.String contactNumber) {
+		this.vehicleNumber = vehicleNumber;
+		this.driverName = driverName;
+		this.contactNumber = contactNumber;
+	}
 
 }

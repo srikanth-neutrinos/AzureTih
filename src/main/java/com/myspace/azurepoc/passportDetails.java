e2cb601acad662e6036783f0150c8aca547cdc08
neutrinos <>
2021-02-25 17:20:26 +0530

    {/src/main/java/com/myspace/azurepoc/passportDetails.java}

--- src/main/java/com/myspace/azurepoc/passportDetails.java
+++ src/main/java/com/myspace/azurepoc/passportDetails.java
@@ -6,12 +6,47 @@ package com.myspace.azurepoc;
 
 public class passportDetails implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public passportDetails() {
-    }
+	@org.kie.api.definition.type.Label(value = "name")
+	private java.lang.String name;
+	@org.kie.api.definition.type.Label(value = "location")
+	private java.lang.String location;
+	@org.kie.api.definition.type.Label(value = "country")
+	private java.lang.String country;
 
+	public passportDetails() {
+	}
 
+	public java.lang.String getName() {
+		return this.name;
+	}
 
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public java.lang.String getLocation() {
+		return this.location;
+	}
+
+	public void setLocation(java.lang.String location) {
+		this.location = location;
+	}
+
+	public java.lang.String getCountry() {
+		return this.country;
+	}
+
+	public void setCountry(java.lang.String country) {
+		this.country = country;
+	}
+
+	public passportDetails(java.lang.String name, java.lang.String location,
+			java.lang.String country) {
+		this.name = name;
+		this.location = location;
+		this.country = country;
+	}
 
 }
